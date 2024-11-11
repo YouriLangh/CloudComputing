0d4c762374b54a56ca54f34e5172fddd48420423
Bryan-Langhendries <bryan1999langhendries@hotmail.com>
2024-11-11 14:42:24 +0100

    compose working

--- client_order_streamer/main.js
+++ client_order_streamer/main.js
@@ -1,40 +1,45 @@
 const fs = require("fs");
 const split2 = require("split2");
 const amqp = require("amqplib");
+const assert = require("assert");
 
 async function streamOrders(cvs_filepath) {
-    const connection = await amqp.connect("amqp://localhost");
-    const channel = await connection.createChannel();
-    const queue = "orders";
+  const connection = await amqp.connect("amqp://rabbitmq");
+  const channel = await connection.createChannel();
+  const queue = "orders";
 
-    // Ensure the queue exists
-    await channel.assertQueue(queue, { durable: true });
+  // Ensure the queue exists
+  await channel.assertQueue(queue, { durable: true });
 
-    const orderStream = fs.createReadStream(cvs_filepath, { encoding: "utf-8" }).pipe(split2());
+  const orderStream = fs
+    .createReadStream(cvs_filepath, { encoding: "utf-8" })
+    .pipe(split2());
 
-    for await (const line of orderStream) {
-        const order = parseLine(line);
-        channel.sendToQueue(queue, Buffer.from(JSON.stringify(order)), { persistent: true });
-    }
+  for await (const line of orderStream) {
+    const order = parseLine(line);
+    channel.sendToQueue(queue, Buffer.from(JSON.stringify(order)), {
+      persistent: true,
+    });
+  }
 
-    setTimeout(() => {
-        connection.close();
-    }, 500); // Close the connection after a delay to ensure all messages are sent
+  setTimeout(() => {
+    connection.close();
+  }, 500); // Close the connection after a delay to ensure all messages are sent
 }
 
 function parseLine(line) {
-    const fields = line.split(",");
-    assert.equal(fields_array.length, 7, "Expected 7 fields!");
-    return {
-        user_id: fields[0],
-        timestamp_ns: fields[1],
-        price: fields[2],
-        symbol: fields[3],
-        quantity: fields[4],
-        order_type: fields[5],
-        trader_type: fields[6],
-    };
+  const fields = line.split(",");
+  assert.equal(fields.length, 7, "Expected 7 fields!");
+  return {
+    user_id: fields[0],
+    timestamp_ns: fields[1],
+    price: fields[2],
+    symbol: fields[3],
+    quantity: fields[4],
+    order_type: fields[5],
+    trader_type: fields[6],
+  };
 }
 
-const cvs_filepath = "data/market_simulation_orders-1h.csv";
+const cvs_filepath = "/app/data/market_simulation_orders-1h.csv";
 streamOrders(cvs_filepath);
