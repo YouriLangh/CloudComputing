836d97c33eb6b45023c1f4d5fc7563de5b791d20
Bryan-Langhendries <bryan1999langhendries@hotmail.com>
2024-11-12 12:03:02 +0100

    data publisher connected

--- market_data_publisher/main.js
+++ market_data_publisher/main.js
@@ -4,44 +4,44 @@ const kafka = new Kafka({ brokers: ["kafka:9092"] });
 const consumer = kafka.consumer({ groupId: "market_data_group" });
 
 async function startMarketDataPublisher() {
-    await consumer.connect();
-    
-    // Subscribe to both 'orders' and 'order_fills' topics
-    await consumer.subscribe({ topic: "orders", fromBeginning: true });
-    await consumer.subscribe({ topic: "order_fills", fromBeginning: true });
+  await consumer.connect();
 
-    console.log("Market Data Publisher is now consuming from Kafka topics...");
+  // Subscribe to both 'orders' and 'order_fills' topics
+  await consumer.subscribe({ topic: "orders", fromBeginning: true });
+  await consumer.subscribe({ topic: "order_fills", fromBeginning: true });
 
-    await consumer.run({
-        eachMessage: async ({ topic, partition, message }) => {
-            const data = JSON.parse(message.value.toString());
+  console.log("Market Data Publisher is now consuming from Kafka topics...");
 
-            if (topic === "orders") {
-                console.log("Received new order:", data);
-                processOrder(data);
-            } else if (topic === "order_fills") {
-                console.log("Received execution fill:", data);
-                processFill(data);
-            }
-        },
-    });
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      const data = JSON.parse(message.value.toString());
+
+      if (topic === "orders") {
+        console.log("Received new order:", data);
+        processOrder(data);
+      } else if (topic === "order_fills") {
+        console.log("Received execution fill:", data);
+        processFill(data);
+      }
+    },
+  });
 }
 
 function processOrder(order) {
-    // Process new order data for the order book or any real-time UI updates
-    console.log("Processing order for dashboard:", order);
-    publishToDashboard(order, "order");
+  // Process new order data for the order book or any real-time UI updates
+  console.log("Processing order for dashboard:", order);
+  publishToDashboard(order, "order");
 }
 
 function processFill(fill) {
-    // Process fill data for the order book or any real-time UI updates
-    console.log("Processing fill for dashboard:", fill);
-    publishToDashboard(fill, "fill");
+  // Process fill data for the order book or any real-time UI updates
+  //console.log("Processing fill for dashboard:", fill);
+  publishToDashboard(fill, "fill");
 }
 
 function publishToDashboard(data, type) {
-    // Code to publish data to the dashboard (e.g., via WebSocket)
-    console.log(`Publishing ${type} to dashboard:`, data);
+  // Code to publish data to the dashboard (e.g., via WebSocket)
+  // console.log(`Publishing ${type} to dashboard:`, data);
 }
 
 startMarketDataPublisher();
--- order_manager/main.js
+++ order_manager/main.js
@@ -1,84 +1,93 @@
 const amqp = require("amqplib");
 const { Kafka } = require("kafkajs");
-const { MatchingEngine, EngineOrder } = require('/app/matching-engine');
+const { MatchingEngine, EngineOrder } = require("/app/matching-engine");
 
-let matchingEngine = new MatchingEngine(['AAPL', 'GOOGL', 'MSFT', 'AMZN']);
+let matchingEngine = new MatchingEngine(["AAPL", "GOOGL", "MSFT", "AMZN"]);
 
 // Kafka setup
 const kafka = new Kafka({ brokers: ["kafka:9092"] });
 const producer = kafka.producer();
 
 async function connectKafkaProducer() {
-    await producer.connect();
-    console.log("Connected to Kafka producer.");
+  await producer.connect();
+  console.log("Connected to Kafka producer.");
 }
 
 // Execution handler for the matching engine
 async function executionHandler(ask_executions, bid_executions) {
-    // console.log("Ask executions:", ask_executions);
-    // console.log("Bid executions:", bid_executions);
+  // console.log("Ask executions:", ask_executions);
+  // console.log("Bid executions:", bid_executions);
 
-    // Publish executions to Kafka Streams
-    for (const execution of ask_executions) {
-        await producer.send({
-            topic: "order_fills",
-            messages: [{ value: JSON.stringify({ ...execution, type: "ask" }) }],
-        });
-    }
-    for (const execution of bid_executions) {
-        await producer.send({
-            topic: "order_fills",
-            messages: [{ value: JSON.stringify({ ...execution, type: "bid" }) }],
-        });
-    }
+  // Publish executions to Kafka Streams
+  for (const execution of ask_executions) {
+    await producer.send({
+      topic: "order_fills",
+      messages: [{ value: JSON.stringify({ ...execution, type: "ask" }) }],
+    });
+  }
+  for (const execution of bid_executions) {
+    await producer.send({
+      topic: "order_fills",
+      messages: [{ value: JSON.stringify({ ...execution, type: "bid" }) }],
+    });
+  }
 }
 
 const ORDER_MANAGER_QUEUE = "order_manager_queue"; // Queue for validated orders
 const RABBITMQ_URL = "amqp://rabbitmq"; // RabbitMQ connection URL
 
+// TODO: why does this start at 1 and not 2?
 class SequentialNumberGenerator {
-    constructor(start = 1) {
-        this.current = start;
-    }
+  constructor(start = 1) {
+    this.current = start;
+  }
 
-    getNext() {
-        return this.current++; // returns the current number and then increments
-    }
+  getNext() {
+    return this.current++; // returns the current number and then increments
+  }
 }
 
 const seqGen = new SequentialNumberGenerator();
 
 async function consumeAndForwardOrders() {
-    try {
-        const connection = await amqp.connect(RABBITMQ_URL);
-        const channel = await connection.createChannel();
+  try {
+    const connection = await amqp.connect(RABBITMQ_URL);
+    const channel = await connection.createChannel();
 
-        await channel.assertQueue(ORDER_MANAGER_QUEUE, { durable: true });
-        console.log("Order manager is now consuming orders from RabbitMQ...");
+    await channel.assertQueue(ORDER_MANAGER_QUEUE, { durable: true });
+    console.log("Order manager is now consuming orders from RabbitMQ...");
 
-        // Start consuming messages
-        channel.consume(ORDER_MANAGER_QUEUE, async (msg) => {
-            if (msg !== null) {
-                const { price, symbol, quantity, order_type } = JSON.parse(msg.content.toString());
+    // Start consuming messages
+    channel.consume(ORDER_MANAGER_QUEUE, async (msg) => {
+      if (msg !== null) {
+        const { price, symbol, quantity, order_type } = JSON.parse(
+          msg.content.toString()
+        );
 
-                // Create an EngineOrder instance from the parsed data
-                const order = new EngineOrder(symbol, order_type, parseFloat(price), parseInt(quantity), seqGen.getNext());
+        // Create an EngineOrder instance from the parsed data
+        const order = new EngineOrder(
+          symbol,
+          order_type,
+          parseFloat(price),
+          parseInt(quantity),
+          seqGen.getNext()
+        );
 
-                // console.log(`Order received: ${order.side} ${order.quantity} of ${order.symbol} at ${order.price}`);
-                await producer.send({
-                  topic: "orders",
-                  messages: [{ value: JSON.stringify(order) }],
-              });
-
-                matchingEngine.execute(order, executionHandler);
-
-                // Acknowledge the message upon successful processing
-                channel.ack(msg);
-            }
+        // console.log(`Order received: ${order.side} ${order.quantity} of ${order.symbol} at ${order.price}`);
+        await producer.send({
+          topic: "orders",
+          messages: [{ value: JSON.stringify(order) }],
         });
-    } catch (error) {
-        console.error("Failed to consume and forward orders:", error);
-    }
+
+        matchingEngine.execute(order, executionHandler);
+
+        // Acknowledge the message upon successful processing
+        channel.ack(msg);
+      }
+    });
+  } catch (error) {
+    console.error("Failed to consume and forward orders:", error);
+  }
 }
 
 // Connect to Kafka and start consuming orders
